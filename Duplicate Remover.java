1f10abad495272df2be82a19a40c8a221d5b4fee
Fardin Hasan <wizardfire77@gmail.com>
2026-02-12 01:57:15 +0600

    Update Duplicate Remover.java

--- Duplicate Remover.java
+++ Duplicate Remover.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class lab_07_cw_4{
+public class main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the length of the array:");
