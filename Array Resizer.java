07695dfa1fa0c95e43806f70c16cc0ad1158acfc
Fardin Hasan <wizardfire77@gmail.com>
2026-02-12 01:36:54 +0600

    Update Array Resizer.java

--- Array Resizer.java
+++ Array Resizer.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class mahi_rak_prob{
+public class mahi{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the length of an array:");
