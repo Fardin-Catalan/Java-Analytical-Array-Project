95202f59614d319a8d8670f89617d875f202b684
Fardin Hasan <wizardfire77@gmail.com>
2026-02-12 01:45:06 +0600

    Create Descending Selection Sorting.java

---
+++ Descending Selection Sorting.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class lab_07_cw_5{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the length of the array:");
+        int n=sc.nextInt();
+        int[] old=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.print("Enter a number:");
+            old[i]=sc.nextInt();
+        }
+        for(int i=0;i<old.length-1;i++){
+            for(int j=i+1;j<old.length;j++){
+                if(old[j]>old[i]){
+                    int temp=old[i];
+                    old[i]=old[j];
+                    old[j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<old.length;i++){
+            System.out.print(old[i]+" ");
+        }
+    }
+}
